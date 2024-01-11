5f7be361edcdbf82ac8970a23e14abd6619fdd25
ALVIN MARCO CRISTY TAMPUBOLON <alvinmarco8@gmail.com>
2024-01-11 12:10:48 +0700

    update

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('home/', views.formBantek, name='formBantek'),
-    path('', views.login, name='login'),
+    path('', views.formBantek, name='formBantek'),
+    path('home/', views.login, name='login'),
     path('tiket/', views.tiketBantek, name='tiketBantek'),
     path('tiket/daftar/', views.daftarTiket, name='daftarTiket'),
 ]
